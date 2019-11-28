528de8fe454ad4347c247a413e661e00539c2610
abdulla oksum <a.oksum@live.dk>
2019-11-28 14:28:12 +0100

    Kriptoda dil degisimi

--- FootballManager.Bll/Helpers/ToPasswordRepository.cs
+++ FootballManager.Bll/Helpers/ToPasswordRepository.cs
@@ -6,17 +6,17 @@
 
     public static class ToPasswordRepository
     {
-        public static byte[] ByteDonustur(string deger)
+        public static byte[] ByteDonustur(string value)
         {
 
             UnicodeEncoding byteConverter = new UnicodeEncoding();
-            return byteConverter.GetBytes(deger);
+            return byteConverter.GetBytes(value);
 
         }
 
-        public static byte[] Byte8(string deger)
+        public static byte[] Byte8(string value)
         {
-            char[] arrayChar = deger.ToCharArray();
+            char[] arrayChar = value.ToCharArray();
             byte[] arrayByte = new byte[arrayChar.Length];
             for (int i = 0; i < arrayByte.Length; i++)
             {
@@ -24,33 +24,33 @@
             }
             return arrayByte;
         }
-        public static string Md5(string strGiris)
+        public static string Md5(string incomestring)
         {
-            if (string.IsNullOrEmpty(strGiris))
+            if (string.IsNullOrEmpty(incomestring))
             {
                 throw new ArgumentNullException(@"Şifrelenecek veri yok");
             }
             else
             {
-                MD5CryptoServiceProvider sifre = new MD5CryptoServiceProvider();
-                byte[] sifrebytes = sifre.ComputeHash(Encoding.UTF8.GetBytes(strGiris));
+                MD5CryptoServiceProvider password = new MD5CryptoServiceProvider();
+                byte[] passwordByte = password.ComputeHash(Encoding.UTF8.GetBytes(incomestring));
                 var sb = new StringBuilder();
-                foreach (byte b in sifrebytes)
+                foreach (byte b in passwordByte)
                     sb.Append(b.ToString("x2").ToLower());
 
                 return sb.ToString();
             }
         }
-        public static string Sha1(string strGiris)
+        public static string Sha1(string incomestring)
         {
-            if (string.IsNullOrEmpty(strGiris))
+            if (string.IsNullOrEmpty(incomestring))
             {
                 throw new ArgumentNullException(@"Şifrelenecek veri yok.");
             }
             else
             {
                 SHA1CryptoServiceProvider sifre = new SHA1CryptoServiceProvider();
-                 byte[] sifrebytes = sifre.ComputeHash(Encoding.UTF8.GetBytes(strGiris));
+                 byte[] sifrebytes = sifre.ComputeHash(Encoding.UTF8.GetBytes(incomestring));
                  var sb = new StringBuilder();
                  foreach (byte b in sifrebytes)
                      sb.Append(b.ToString("x2").ToLower());
